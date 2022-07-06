8a148387479aeadcb71e4d77819b8bff50d51f87
Jason Elkin <jasonelkin86@gmail.com>
2022-07-06 22:49:36 +0100

    Add project files.

---
+++ HangfireUmbraco/Events/RegisterHangfire.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire;
+using HangfireUmbraco.Services;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace HangfireUmbraco.Events
+{
+    public class RegisterHangfire : INotificationHandler<ContentPublishingNotification>
+    {
+        public void Handle(ContentPublishingNotification notification)
+        {
+            RecurringJob.AddOrUpdate<ServiceThatUsesUmbracoContext>(x => x.DoSomethingWithUmbracoContext() , "* * * * *");
+        }
+    }
+}
---
+++ HangfireUmbraco/Services/ServiceThatUsesUmbracoContext.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Cms.Core.Web;
+
+namespace HangfireUmbraco.Services
+{
+    public class ServiceThatUsesUmbracoContext
+    {
+        private readonly IUmbracoContextFactory ctxFactory;
+
+        public ServiceThatUsesUmbracoContext(IUmbracoContextFactory ctxFactory)
+        {
+            this.ctxFactory = ctxFactory;
+        }
+
+        public void DoSomethingWithUmbracoContext()
+        {
+            using (var ctxRef = ctxFactory.EnsureUmbracoContext())
+            {
+                var contentAtRoot = ctxRef.UmbracoContext.Content!.GetAtRoot();
+
+                var ids = string.Join(", ", contentAtRoot.Select(x => x.Id));
+
+                Console.WriteLine($"Content IDs at root: {ids}");
+            }
+        }
+    }
+}
